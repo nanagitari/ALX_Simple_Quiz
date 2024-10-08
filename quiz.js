03e04cd47096fcb8d4c2b0692bc7c662b091f61a
nanagitari <nanagitari18@gmail.com>
2024-10-08 16:19:57 +0300

    added 2 files

---
+++ quiz.js
@@ -0,0 +1,18 @@
+function checkAnswer () {
+    let correctAnswer= "4" ;
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+
+    
+    let feedback= document.getElementById('feedback');
+
+    if (userAnswer==correctAnswer){
+        feedback.textContent= "Correct! Well done";
+    } else {
+        feedback.textContent= "Incorrect! Try Again";
+    }
+
+}
+    let submitButton = document.getElementById('submit-answer');
+    submitButton.addEventListener('click',checkAnswer);
+
+
