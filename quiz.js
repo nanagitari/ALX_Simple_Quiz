9a8186fafb14b70d42527f46a52aed191a7aef56
nanagitari <nanagitari18@gmail.com>
2024-10-09 11:54:31 +0300

    contents added

--- quiz.js
+++ quiz.js
@@ -2,12 +2,12 @@ function checkAnswer () {
     let correctAnswer= "4" ;
     let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
-    
     let feedback= document.getElementById('feedback');
 
     if (userAnswer==correctAnswer){
         feedback.textContent= "Correct! Well done";
-    } else {
+    } 
+    else {
         feedback.textContent= "Incorrect! Try Again";
     }
 
