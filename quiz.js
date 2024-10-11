89aef9b6b8b8e0c1299ce15150182bff9aac1d2f
nanagitari <51341755+nanagitari@users.noreply.github.com>
2024-10-11 15:59:25 +0300

    made changes

--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@ function checkAnswer () {
     let feedback= document.getElementById('feedback');
 
     if (userAnswer === correctAnswer){
-        feedback.textContent= "Correct! Well done";
+        feedback.textContent= "Correct! Well done.";
     } 
     else {
         feedback.textContent= "Incorrect! Try Again.";
